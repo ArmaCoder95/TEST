565c208f2e78a70f00033cde6f8655d7648d958e
Marin Flores <mflores@plusap.pe>
2023-05-04 20:32:30 +0000

    Ajuste mainController

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
 
         return Controller.extend("ztest.test.controller.Main", {
             onInit: function () {
-
+                let oThat = this;
             }
         });
     });
